6a4b23136138639a2ae664418f218cd91131e7de
korea7030 <korea7030@naver.com>
2020-04-20 23:45:46 +0900

    recap refactoring code

--- src/index.js
+++ src/index.js
@@ -8,15 +8,19 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 // store에 전달할 함수 생성
 const countModifier = (count = 0, action) => {  // initialize state
   // console.log(count, action); // 0 {type: "@@redux/INITl.9.7.q.0.g"}
-  if (action.type === "ADD") {
-    return count +1;
-  } else if (action.type === "MINUS"){
-    return count -1;
-  } else {
-    return count;
+  switch(action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -30,5 +34,5 @@ const onChange = () => {
 countStore.subscribe(onChange);
 // console.log(countStore); // dispatch, subscribe, getState, replaceReducer, Symbol 정보
 
-plus.addEventListener("click", () => countStore.dispatch({type: "ADD"}));
-minus.addEventListener("click", () => countStore.dispatch({type: "MINUS"}));
+plus.addEventListener("click", () => countStore.dispatch({type: ADD}));
+minus.addEventListener("click", () => countStore.dispatch({type: MINUS}));
